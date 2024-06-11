87a66684d7889e257bb4ce5abcc6f52d18dbd3cf
yshong1998 <yshong1998@gmail.com>
2024-06-11 11:23:35 +0900

    initial commit

---
+++ src/test/java/com/example/mygithubprac/MygithubpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mygithubprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygithubpracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
