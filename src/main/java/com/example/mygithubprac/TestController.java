9fd3828f5d2f72c145a49bbe1b5216db7ae9be7f
yshong1998 <yshong1998@gmail.com>
2024-06-12 15:16:04 +0900

    test

--- src/main/java/com/example/mygithubprac/TestController.java
+++ src/main/java/com/example/mygithubprac/TestController.java
@@ -1,5 +1,6 @@
 package com.example.mygithubprac;
 
+import com.example.mygithubprac.entity.TestEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,11 @@ public class TestController {
     @GetMapping("/api/test")
     public String testApi(){
         return "testSuccess";
+    }
+
+    @GetMapping("/api/entityTest")
+    public TestEntity testEntityApi(){
+        return new TestEntity("테스트메세지");
     }
 
 }
---
+++ src/main/java/com/example/mygithubprac/entity/TestEntity.java
@@ -0,0 +1,16 @@
+package com.example.mygithubprac.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestEntity {
+
+    private String testString;
+
+}
