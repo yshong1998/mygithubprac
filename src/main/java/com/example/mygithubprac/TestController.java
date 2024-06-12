67dcd343419d40f569ced574736d27cf1d811ea1
yshong1998 <yshong1998@gmail.com>
2024-06-12 14:46:19 +0900

    test

---
+++ src/main/java/com/example/mygithubprac/TestController.java
@@ -0,0 +1,14 @@
+package com.example.mygithubprac;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String testApi(){
+        return "testSuccess";
+    }
+
+}
