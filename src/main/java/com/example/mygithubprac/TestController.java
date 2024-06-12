3e0fb977436b760eef34eb1e220ec5312ecb5f00
yshong1998 <yshong1998@gmail.com>
2024-06-13 04:43:13 +0900

    test

--- src/main/java/com/example/mygithubprac/TestController.java
+++ src/main/java/com/example/mygithubprac/TestController.java
@@ -1,20 +1,38 @@
 package com.example.mygithubprac;
 
 import com.example.mygithubprac.entity.TestEntity;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/api/test")
-    public String testApi(){
-        return "testSuccess";
+    @Value("${server.env}")
+    private String env;
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${server.serverAddress}")
+    private String serverAddress;
+    @Value("${serverName}")
+    private String serverName;
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck(){
+        Map<String, String> responseData = new TreeMap<>();
+        responseData.put("serverName", serverName);
+        responseData.put("serverAddress", serverAddress);
+        responseData.put("serverPort", serverPort);
+        responseData.put("env", env);
+        return ResponseEntity.ok(responseData);
     }
 
-    @GetMapping("/api/entityTest")
-    public TestEntity testEntityApi(){
-        return new TestEntity("테스트메세지");
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv(){
+        return ResponseEntity.ok(env);
     }
-
 }
